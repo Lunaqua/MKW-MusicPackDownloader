0a57f5a24b54e754c868f7ea9a47affc8a25738a
Lunaqua <lunaqua@protonmail.com>
2021-08-30 21:43:05 +0100

    Edited JSON key syntax. Added function to download files.

--- main.sh
+++ main.sh
@@ -79,7 +79,7 @@ function user_file_check {
 # Checks to see if the selected pack is the one the user wants
 
 fileDirect="./JSON/$1"
-        printf "\nMusic Pack Information"
+        printf "\nMusic Pack Information\n"
         echo "---"
         echo "Name: $(jq -r '.name' $fileDirect)"
         echo "Author: $(jq -r '.author' $fileDirect)"
@@ -97,6 +97,23 @@ fileDirect="./JSON/$1"
 
 }
 
+function init {
+# Simple function to initialise required folders
+    mkdir downloads
+    mkdir conversion
+    mkdir My\ Stuff
+    mkdir logs
+
+}
+
+function deinit {
+# Cleanup
+    rm -rf downloads
+    rm -rf conversion
+    rm -rf My\ Stuff
+
+}
+
 function json_process {
 # This function process the json file to calculate certain parameters.
     # Firstly, the number of tracks filled is calculated, this will ensure that the JSON is complete.
@@ -110,7 +127,7 @@ fi
     # JSON Key Parameters
         # "course-name"     - Optional, acts as a guide to the track listing
         # "enabled"         - Enables the track (1 or 0, if 0, all other keys are optional)
-        # "download-type"   - Can be:
+        # "downloadtype"   - Can be:
             # 0 - Standard wget download
             # 1 - smashcustommusic download (No conversion)
             # 2 - youtube download (via yt-dlp)
@@ -123,13 +140,58 @@ fi
         # "volume"          - Volume increase of track in dB
         # "speed"           - Speed increase (defaults to 1.10x)
         
+    # Obtain all information needed.
 
+courseName="$(jq -r '.Tracks | .['$loop'].Track' $dictionary)"
+fileName="$(jq -r '.Tracks | .['$loop'].Filename' $dictionary)"
+channels="$(jq -r '.Tracks | .['$loop'].Tracks' $dictionary)"
+title="$(jq -r '.tracks | .['$loop'].title' $fileDirect)"
+enabled="$(jq -r '.tracks | .['$loop'].enabled' $fileDirect)"
+downloadType="$(jq -r '.tracks | .['$loop'].downloadtype' $fileDirect)"
+game="$(jq -r '.tracks | .['$loop'].game' $fileDirect)"
+length="$(jq -r '.tracks | .['$loop'].length' $fileDirect)"
+link="$(jq -r '.tracks | .['$loop'].link' $fileDirect)"
+loop="$(jq -r '.tracks | .['$loop'].loop' $fileDirect)"
+volume="$(jq -r '.tracks | .['$loop'].volume' $fileDirect)"
+speed="$(jq -r '.tracks | .['$loop'].speed' $fileDirect)"
+
+if test $enabled = "1"; then
+    printf "\n\n"
+    echo " -- "
+    echo "Course Name: $courseName"
+    echo "Title: $title"
+    echo "Game: $game"
+    echo "Length: $length"
+    echo " -- "
+fi
+
+if test $enabled = "0"; then
+        return 1
+    fi
 
 }
 
-function music_downloader {
-# This function performing the track downloads.
-    echo "TODO";
+function downloader_chooser {
+# This function decides which download method is used, and downloads the file.
+    
+    case $downloadType in
+        0)
+        wget "$link" -O ./downloads/output_$loop -o ./logs/log_$loop.txt
+        ;;
+        
+        1)
+        wget "https://smashcustommusic.net/brstm/$link" -O ./downloads/output_$loop$extension -o ./logs/log_$loop.txt
+        ;;
+        
+        2)
+        yt-dlp "$link" -f 251 -o ./downloads/output_$loop.webm > /logs/log_$loop.txt
+        ;;
+        
+        *)
+        echo "Invalid Download Method"
+        exit 4;
+        ;;
+    esac
 }
 
 # - - - - - - - - - - - - - - - -
@@ -139,8 +201,20 @@ function music_downloader {
 file=""
 extension=".brstm"
 dictionary="trackListing.json"
+loop=0
 
 file_check
 file_selection_menu
 user_file_check $file
+printf "\n -- beginning download and construction process -- "
+init
+
 json_process
+#if test $? -eq 1; then
+#    echo "do next loop number"
+#fi
+
+downloader_chooser
+#deinit
+
+exit 0;
